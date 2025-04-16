a464432e672661e20ab6ff813d61f1da9e17ae05
Heelim Hong <heelim.korea@gmail.com>
2025-04-16 13:36:53 +0000

    add trace_generator.py by xinyuema

--- scripts/run_no_prefetch.sh
+++ scripts/run_no_prefetch.sh
@@ -2,11 +2,11 @@
 
 export CUDA_VISIBLE_DEVICES=0
 export VLLM_CONFIGURE_LOGGING=1
-export VLLM_LOGGING_CONFIG_PATH=/home/xinyuema/vllm/configs/test_no_prefetch_logging.json
-config_file=/home/xinyuema/vllm/samples/large_new_request.json
-mkdir -p /home/xinyuema/vllm/outputs/test_no_prefetch
-> /home/xinyuema/vllm/outputs/test_no_prefetch/vllm_msg.log
+export VLLM_LOGGING_CONFIG_PATH=/home/heelim/vllm/configs/test_no_prefetch_logging.json
+config_file=/home/heelim/vllm/samples/large_new_request.json
+mkdir -p /home/heelim/vllm/outputs/test_no_prefetch
+> /home/heelim/vllm/outputs/test_no_prefetch/vllm_msg.log
 python ../examples/test_distN.py \
     --config=$config_file \
     --prefetch_mode=none \
-    --output_log=/home/xinyuema/vllm/outputs/test_no_prefetch/output.log \
+    --output_log=/home/heelim/vllm/outputs/test_no_prefetch/output.log \
