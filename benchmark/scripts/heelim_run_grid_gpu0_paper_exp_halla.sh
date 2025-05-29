703fdadb40446bdfd928ccae13ec480764379e91
heelim hong <heelim.korea@gmail.com>
2025-05-29 05:01:10 +0000

    add output files

--- benchmark/scripts/heelim_run_grid_gpu0_paper_exp_halla.sh
+++ benchmark/scripts/heelim_run_grid_gpu0_paper_exp_halla.sh
@@ -15,12 +15,12 @@ ROOT="/home/heelim/vllm"
 FIGURE_ONLY=$1                      # 0 → 실행 + 그림, 1 → 그림만
 EXP_LIST=(paper_main_exp)
 # METHOD_LIST=(Ours Flexgen)
-METHOD_LIST=(Ours)
+METHOD_LIST=(DistNSingle)
 TRACE_CFG_DIR="${ROOT}/benchmark/selected_traces/"
 # TRACE_LIST=(bim50_hi_ov78_scaled_debugging)
-# TRACE_LIST=(both_dyn_low both_dyn_mid both_dyn_high)
+TRACE_LIST=(both_static_low both_static_mid both_static_high batch_dyn_low batch_dyn_mid batch_dyn_high token_dyn_low token_dyn_mid token_dyn_high both_dyn_low both_dyn_mid both_dyn_high both_static_veryhigh batch_dyn_veryhigh token_dyn_veryhigh both_dyn_veryhigh)
 # TRACE_LIST=(batch_dyn_high batch_dyn_veryhigh)
-TRACE_LIST=(token_dyn_low token_dyn_mid token_dyn_high token_dyn_veryhigh)
+# TRACE_LIST=(token_dyn_low token_dyn_mid token_dyn_high token_dyn_veryhigh)
 # TRACE_LIST=(batch_dyn_low batch_dyn_mid batch_dyn_high batch_dyn_veryhigh)
 
 METHOD_CFG_FILE="${ROOT}/benchmark/scripts/supported_methods.json"
@@ -29,7 +29,7 @@ PLOTTER="${ROOT}/benchmark/data_analysis/metrics_plot.py"
 
 # ★ 실험할 slo_ratio 값들만 여기에 나열하면 됩니다.
 # SLO_RATIO_LIST=(1.5 2.0 2.5 3.0)
-SLO_RATIO_LIST=(1.5)
+SLO_RATIO_LIST=(2.5)
 ###############################################################################
 # MAIN LOOP: SLO ➔ EXP ➔ METHOD ➔ TRACE                                      #
 ###############################################################################
